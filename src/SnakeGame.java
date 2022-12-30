bf15609988e73ff88ca6e2e0d025b4bb3eb778c5
Jaikishan <jaikishangupta754@gmail.com>
2022-12-30 12:22:40 +0530

    Initial commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,13 @@
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		
+//		GameFrame frame=new GameFrame();  //both are same
+		new GameFrame();                // its a shortcut: we can remove the some part if we don't want to use frame in 
+		
+
+	}
+
+}
